4cdd946393933d582dd06c13780120840435de26
james-muriithi <muriithijames556@gmail.com>
2020-01-09 20:00:08 +0300

    added a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -42,42 +42,39 @@ class _MyHomePageState extends State<MyHomePage> {
   ];
 
   List<Widget> _buttons() {
-     List<Widget> myButtons = List.generate(_map.length, (i) {
+    var label = 0;
+    List<Widget> myButtons = List.generate(_map.length, (i) {
       var row = _map[i].split('');
+      List<Widget> myList = [];
+      var counter = 0;
       row.forEach((item) {
-        if(item.toString().trim() == '_'){
-          return Container(
-                  width: 50,
-                );
+        var seat = new Seat((label + 1).toString(), false,
+            MediaQuery.of(context).size.width / 7.5);
+        if (item.toString().trim() == '_' && counter % 2 == 0) {
+          // print(object)
+          myList.add(Container(
+            width: MediaQuery.of(context).size.width / 7.5,
+            margin: EdgeInsets.only(left: 3.0, right: 3.0, bottom: 3.0),
+          ));
+        } else if (item.toString().trim() == '_' && i == 0 && counter == 5) {
+          myList.add(Container(
+            width: MediaQuery.of(context).size.width / 7.5,
+            child: seat._driver(),
+          ));
+        } else if (item.toString().trim() == '_') {
+          myList.add(Container(
+            width: MediaQuery.of(context).size.width / 7.5,
+            margin: EdgeInsets.only(left: 3.0, right: 3.0, bottom: 3.0),
+          ));
+        } else {
+          label++;
+          myList.add(seat);
         }
-        return _seat('buttonText');
+        counter++;
       });
+      return new Row(children: myList);
     });
     return myButtons;
-  }
-
-  Widget _seat(String buttonText) {
-    Color _mycolor = Color.fromRGBO(150, 150, 150, 1);
-    return Container(
-      width: 50,
-      margin: EdgeInsets.only(left: 5.0, right: 5.0, bottom: 3.0),
-      child: MaterialButton(
-        color: _mycolor,
-        highlightColor: Color.fromRGBO(48, 119, 1, 1),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        padding: EdgeInsets.only(top: 12.0, bottom: 12.0),
-        child: new Text(
-          buttonText,
-          style: TextStyle(
-              fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.white),
-        ),
-        onPressed: () {
-          setState(() {
-            // _mycolor = Color.fromRGBO(48, 119, 1, 1);
-          });
-        },
-      ),
-    );
   }
 
   @override
@@ -87,97 +84,13 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Container(
-        margin: const EdgeInsets.only(top: 25.0, left: 10.0, right: 10.0),
+        margin: EdgeInsets.only(left: 25.0, right: 5.0),
         color: Colors.white30,
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                _seat('1'),
-                _seat('2'),
-                Container(
-                  width: 30,
-                ),
-                Container(
-                  width: 50,
-                ),
-                Container(
-                  width: 50,
-                ),
-                Container(
-                  width: 50,
-                  child: Image.network(
-                      'https://james-muriithi.github.io/bus/images/driver.png'),
-                )
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                _seat('3'),
-                _seat('4'),
-                Container(
-                  width: 30,
-                ),
-                _seat('5'),
-                _seat('6'),
-                _seat('7'),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                _seat('8'),
-                _seat('9'),
-                Container(
-                  width: 30,
-                ),
-                _seat('10'),
-                _seat('11'),
-                _seat('12'),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Container(
-                  width: 60,
-                ),
-                Container(
-                  width: 60,
-                ),
-                Container(
-                  width: 30,
-                ),
-                _seat('13'),
-                _seat('14'),
-                _seat('15'),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                _seat('16'),
-                _seat('17'),
-                Container(
-                  width: 30,
-                ),
-                _seat('18'),
-                _seat('19'),
-                _seat('20'),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                _seat('21'),
-                _seat('22'),
-                Container(
-                  width: 30,
-                ),
-                _seat('23'),
-                _seat('24'),
-                _seat('25'),
-              ],
-            ),
-          ],
+        child: ListView(
+          padding: EdgeInsets.only(bottom: 30.0, top: 40.0),
+          children: <Widget>[Column(children: _buttons())],
         ),
       ),
       bottomNavigationBar: CurvedNavigationBar(
@@ -212,8 +125,51 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class Seat{
-   final String buttonText;
+class Seat extends StatefulWidget {
+  final String buttonText;
   bool changeButtonColor;
-  Seat({this.buttonText,this.changeButtonColor});
+  final double width;
+  Seat(this.buttonText, this.changeButtonColor, this.width);
+
+  Widget _driver() {
+    return Image.network(
+        'https://james-muriithi.github.io/bus/images/driver.png');
+  }
+
+  @override
+  MySeatState createState() => MySeatState();
+}
+
+class MySeatState extends State<Seat> {
+  @override
+  Widget build(BuildContext context) {
+    Widget _seat() {
+      Color _mycolor = Color.fromRGBO(150, 150, 150, 1);
+      return Container(
+        width: widget.width,
+        margin: EdgeInsets.only(left: 3.0, right: 3.0, bottom: 3.0),
+        child: MaterialButton(
+          color: widget.changeButtonColor ? Color.fromRGBO(48, 119, 1, 1) : _mycolor,
+          // highlightColor: changeButtonColor ? Color.fromRGBO(48, 119, 1, 1): Color.fromRGBO(48, 119, 1, 1),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          padding: EdgeInsets.only(top: 12.0, bottom: 12.0),
+          child: new Text(
+            widget.buttonText,
+            style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white),
+          ),
+          onPressed: () {
+            setState(() {
+              widget.changeButtonColor = !widget.changeButtonColor;
+            });
+          },
+        ),
+      );
+    }
+
+    return _seat();
+  }
 }
